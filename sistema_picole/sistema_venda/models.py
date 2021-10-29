33b1ae9806f6566503b7387adb4df7ba9b0933ce
Daniel Moraes <danielbibit@gmail.com>
2021-10-29 03:03:52 -0300

    Add models - #11

--- sistema_picole/sistema_venda/models.py
+++ sistema_picole/sistema_venda/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models.lookups import PostgresOperatorLookup
+from django.db.models.query import prefetch_related_objects
 
-# Create your models here.
+class Picole(models.Model):
+    marca = models.CharField(max_length=100)
+    sabor = models.CharField(max_length=100)
+    calorias = models.IntegerField()
+    peso = models.FloatField()
+    temperatura_ideal = models.IntegerField()
+
+
+class Lote(models.Model):
+    picole = models.ForeignKey(Picole, on_delete=models.CASCADE)
+    estoque = models.IntegerField()
+    vendedor_origem = models.CharField(max_length=100)
+    data_compra = models.DateField()
+    data_validade = models.DateField()
+    quantidade_picoles = models.IntegerField()
+    preco_lote = models.FloatField()
+
+
+class Vendas(models.Model):
+    horario = models.DateField()
+    preco = models.FloatField()
+    local = models.CharField(max_length=100)
+    metodo_pagamento = models.CharField(max_length=100)
+    tipo_picole = models.ForeignKey(Picole, on_delete=models.CASCADE)
+    lote = models.ForeignKey(Lote, on_delete=models.CASCADE)
