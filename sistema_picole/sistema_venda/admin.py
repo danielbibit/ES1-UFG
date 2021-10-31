e21343f07b7c75e48406570170036a28dfdaa4e0
Amanda Barbosa <amanda.barbosa.m1@gmail.com>
2021-10-31 17:32:03 -0300

    inclusão dos models no admin

--- sistema_picole/sistema_venda/admin.py
+++ sistema_picole/sistema_venda/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Lote, Picole, Vendas
+
+admin.site.register(Lote)
+admin.site.register(Picole)
+admin.site.register(Vendas)
