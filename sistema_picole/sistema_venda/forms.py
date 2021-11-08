372c3072a40aeedcc8b10e02a423bc3790fe4e60
Amanda Barbosa <amanda.barbosa.m1@gmail.com>
2021-11-08 10:40:13 -0300

    Visualizar formulario de vendas

--- sistema_picole/sistema_venda/forms.py
+++ sistema_picole/sistema_venda/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
-from .models import Lote, Picole
+from .models import Lote, Picole, Vendas
 
 class LoteForm(forms.ModelForm):
 
     class Meta:
         model = Lote
-        fields = ('estoque', 'vendedor_origem','data_compra','data_validade','quantidade_picoles','preco_lote')
+        fields = ('estoque', 'vendedor_origem','data_compra','data_validade','quantidade_picoles','preco_lote','picole')
 
 
 class PicoleForm(forms.ModelForm):
@@ -14,3 +14,9 @@ class PicoleForm(forms.ModelForm):
     class Meta:
         model = Picole
         fields = ('marca', 'sabor','calorias','peso','temperatura_ideal')
+
+class VendaForm(forms.ModelForm):
+
+    class Meta:
+        model = Vendas
+        fields = ('horario', 'preco','local','metodo_pagamento','lote', 'tipo_picole')
--- sistema_picole/sistema_venda/views.py
+++ sistema_picole/sistema_venda/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Lote, Picole, Vendas
-from .forms import LoteForm, PicoleForm
+from .forms import LoteForm, PicoleForm, VendaForm
 
 def index(request):
     lotes = Lote.objects.all()
@@ -22,6 +22,10 @@ def estoque(request):
 
     return render(request, 'estoque/lista.html',{'lotes': lotes, 'picoles': picoles} )
 
+def adiciona_venda(request):
+    form = VendaForm()
+    return render(request, 'venda/nova_venda.html', {'form': form})
+
 def adiciona_lote(request):
     form = LoteForm()
     return render(request, 'estoque/novo_estoque.html', {'form': form})
@@ -34,4 +38,9 @@ def edit_estoque(request, id):
     estoque = get_object_or_404(Lote, pk=id)
     form = LoteForm(instance=estoque)
     return render(request, 'estoque/edit_estoque.html', {'form': form,'estoque': estoque })
+
+def edit_venda(request, id):
+    venda = get_object_or_404(Vendas, pk=id)
+    form = VendaForm(instance=venda)
+    return render(request, 'venda/edit_venda.html', {'form': form,'venda': venda })
 
